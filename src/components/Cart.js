a99e15d07352a2e93700e5dc06114a3228da84ae
romycavallaro <romycavallaro@gmail.com>
2022-07-01 09:36:10 -0300

    RE entrega 10

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,56 +1,56 @@
-import React from "react";
-import { useContext } from "react";
-import { Link } from "react-router-dom";
-import { contexto } from "./CartContext";
+import React from 'react';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { contexto } from './CartContext';
 
 const Cart = () => {
-
-    const resultado = useContext(contexto)
-    return (
-        
-      <>
+  const resultado = useContext(contexto);
+  console.log(resultado);
+  console.log(resultado.carrito);
+  console.log(resultado.carrito.length);
+  console.log(resultado.carrito.map((item) => item.item.id));
+  return (
+    <>
       {resultado.carrito.lenght < 1 ? (
+        <div>
+          <p>
+            Tu carrito está vacío. Por favor, agregá algún producto para poder
+            continuar.
+          </p>
+          <Link to="/">Volver al inicio</Link>
+        </div>
+      ) : (
+        <div>
+          <div>
+            <p>Producto</p>
+            <p>Precio</p>
+            <p>Cantidad</p>
+            <p>Subtotal</p>
+          </div>
+          <div>
+            {resultado.carrito.map((item) => (
+              <div key={item.item.id}>
                 <div>
-                    <p>Tu carrito está vacío. Por favor, agregá algún producto para poder continuar.</p>
-                    <Link to='/'>Volver al inicio</Link>
+                  <div>Item id: {item.item.id}</div>
+                  <div>{item.item.name}</div>
+                  <div>{item.item.descripcion}</div>
+                  <div>{item.item.price}</div>
+                  <div>{item.item.quantity}</div>
                 </div>
-            ) : (
-                <div>
-                    <div>
-                        <p>Producto</p>
-                        <p>Precio</p>
-                        <p>Cantidad</p>
-                        <p>Subtotal</p>
-                    </div>
-                    <div>
-                        {resultado.carrito.map((item) => (
-                        <div key={item.id}>
-                            <div>
-                                <p>{item.name}</p>
-                                <p>{item.descripcion}</p>
-                                <p>{item.price}</p>
-                                <p>{item.quantity}</p>
-                            </div>
-                            <div>
-                                {parseFloat((item.price * item.quantity).toFixed(2))}
-                            </div>
-                            <p>
-                                <button onClick={() => resultado.removeItem(item.id)} />
-                            </p>
-                        </div>
-                    ))}
-                        <div>
-                            <p>Subtotal: ${resultado.totalPrice}</p>
-                            <p>Total: ${resultado.totalPrice}</p>
-                            <button>Comprar</button>
-                        </div>
-                    </div>
-                </div>
-            )}
-        </>
- )  
-                               
-}
+
+                <button onClick={() => resultado.removeItem(item.item.id)}>Cancelar Item</button>
+              </div>
+            ))}
+            <div>
+              <p>Subtotal: ${resultado.totalPrice}</p>
+              <p>Total: ${resultado.totalPrice}</p>
+              <button>Comprar</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default Cart;
-
--- src/components/CartContext.js
+++ src/components/CartContext.js
@@ -1,40 +1,40 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState } from 'react';
 
-export const contexto = createContext()
-const Provider = contexto.Provider
+export const contexto = createContext();
+const Provider = contexto.Provider;
 
-const MiProvider = ({children}) => {
+const MiProvider = ({ children }) => {
+  const [carrito, setCarrito] = useState([]);
+  const [cantidadDisponible, setcantidadDisponible] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
 
-    const [carrito, setCarrito] = useState([])
-    const [cantidadDisponible, setcantidadDisponible] = useState(0)
-    const [totalPrice, setTotalPrice] = useState(0);
+  const addItem = (item, quantity) => {
+    const ItemAgregado = {
+      item,
+      quantity: quantity,
+    };
 
-    const addItem = (item, quantity) => {
-        const ItemAgregado = {
-            item,
-            quantity : quantity,
-        }
+    const nuevoCarrito = [...carrito];
 
-        const nuevoCarrito = [carrito];
+    if (isInCart(item.id)) {
+      nuevoCarrito.forEach((product) => {
+        if (product.id === item.id) product.quantity += quantity;
+      });
+      setCarrito(nuevoCarrito);
+      setcantidadDisponible(cantidadDisponible + quantity);
+      setTotalPrice(
+        parseFloat((totalPrice + ItemAgregado.price * quantity).toFixed(2))
+      );
+    } else {
+      nuevoCarrito.push({ ...ItemAgregado });
+      setCarrito(nuevoCarrito);
+      setcantidadDisponible(cantidadDisponible + quantity);
+    }
+  };
 
-        if (isInCart(item.id)) {
-            nuevoCarrito.forEach((product) => {
-              if (product.id === item.id) product.quantity += quantity;
-            });
-            setCarrito(nuevoCarrito);
-            setcantidadDisponible(cantidadDisponible + quantity);
-            setTotalPrice(parseFloat((totalPrice + ItemAgregado.price * quantity).toFixed(2)));
-          } else {
-            nuevoCarrito.push({ ItemAgregado });
-            setCarrito(nuevoCarrito);
-            setcantidadDisponible(cantidadDisponible + quantity);
-          }
-        };
-
-
-    const removeItem = (itemId) => {
-      const nuevoCarrito = [carrito];
-      const decreasePriceAndQuantity = nuevoCarrito.find(
+  const removeItem = (itemId) => {
+    const nuevoCarrito = [...carrito];
+    const decreasePriceAndQuantity = nuevoCarrito.find(
       (item) => item.id === itemId
     );
     setTotalPrice(
@@ -45,38 +45,34 @@ const MiProvider = ({children}) => {
         ).toFixed(2)
       )
     );
-    setcantidadDisponible(cantidadDisponible - decreasePriceAndQuantity.quantity);
+    setcantidadDisponible(
+      cantidadDisponible - decreasePriceAndQuantity.quantity
+    );
 
     const newNuevoCarrito = nuevoCarrito.filter((item) => item.id !== itemId);
     setCarrito(newNuevoCarrito);
   };
 
-    const isInCart = (buscarId) => {
-        return carrito.find(({ id }) => id === buscarId) ? true : false;
-      };
+  const isInCart = (buscarId) => {
+    return carrito.find(({ id }) => id === buscarId) ? true : false;
+  };
 
-    const clearItems = () => {
-        setCarrito([]);
-        setcantidadDisponible(0);
-        setTotalPrice(0);
-    }
+  const clearItems = () => {
+    setCarrito([]);
+    setcantidadDisponible(0);
+    setTotalPrice(0);
+  };
 
-    const valorContext = {
-        carrito : carrito,
-        cantidadDisponible : cantidadDisponible,
-        totalPrice : totalPrice,
-        addItem : addItem,
-        removeItem : removeItem,
-        clearItems : clearItems,
-    };
+  const valorContext = {
+    carrito: carrito,
+    cantidadDisponible: cantidadDisponible,
+    totalPrice: totalPrice,
+    addItem: addItem,
+    removeItem: removeItem,
+    clearItems: clearItems,
+  };
 
+  return <Provider value={valorContext}>{children}</Provider>;
+};
 
-    return (
-        <Provider value={valorContext}>
-            {children}
-        </Provider>
-
-    )
-}
-
-export default MiProvider 
+export default MiProvider;
